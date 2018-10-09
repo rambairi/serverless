282ea97a323e38c5b1b67490f39f660c8f9a5336
ramprasad.bairi <ramprasad.bairi@ggktech.com>
2018-10-09 12:06:58 +0530

    sam template commit

--- src/main/java/aws/sam/ServerLessAppHandler.java
+++ src/main/java/aws/sam/ServerLessAppHandler.java
@@ -4,12 +4,14 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import org.apache.log4j.Logger;
 
-public class ServerLessAppHandler implements RequestHandler<String, String> {
+import java.util.Map;
+
+public class ServerLessAppHandler implements RequestHandler<Map<String,Object>, String> {
 
     private static final Logger log = Logger.getLogger(ServerLessAppHandler.class);
 
     @Override
-    public String handleRequest(String request, Context context) {
+    public String handleRequest(Map<String,Object> request, Context context) {
         log.info("Executing server less application model.");
         return "success";
     }
